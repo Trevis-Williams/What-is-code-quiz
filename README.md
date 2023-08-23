5973c00fc9613e000d6dd74ad8d9d71108bbb504
Trevis Williams <rst1426@gmail.com>
2023-08-23 13:07:56 -0700

    fixed everything after whatevr i did in the last commit broke everything. stupid

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
 # What-is-code-quiz
 
 ## description 
-
+This is a simple quiz! There is a leaderbored and a points system. The faster you comple the quiz and the more acrutaly the better you score will be!!!
 
 ## screenshots
 
 
-  ## help 
+## Deployed page
+
+
   
--- assets/script.js
+++ assets/script.js
@@ -1,39 +1,3 @@
-//make it so when it hits 0 seocnds it links to leaderbored 
-//make it so whenyou input somthing into the leaderbored it saves it 
-// document.addEventListener("DOMContentLoaded", function() {
-//     var timeEl = document.getElementById('time-el');
-//     var secondsLeft = 75;
-
-
-//     //come back to need to make it so timer stop at 0 and negitives. 
-//     function updateTimer() {
-//       timeEl.textContent = secondsLeft;
-//       if (secondsLeft === 0) {
-//         clearInterval(timerInterval);
-//         // Call a function or perform an action when the timer reaches 0
-//         window.location.href = 'leaderbored.html'
-//       }else{
-//         secondsLeft--;
-//       }
-//     }
-  
-//     function sendMessage(message) {
-//       console.log(message);
-//       // You can replace this with any action you want to perform when the timer reaches 0
-//     }
-  
-//     var timerInterval;
-  
-//     // Start the timer when the page loads
-//     startTimer(); 
-    
-   
-//     function startTimer() {
-//       // Call the updateTimer function every second/
-//       timerInterval = setInterval(updateTimer, 1000);
-//     }
-//   });
-
 
 // document.addEventListener("DOMContentLoaded", function() {
 //   var timeEl = document.getElementById('time-el');
@@ -327,101 +291,200 @@
 
 
 
+// document.addEventListener("DOMContentLoaded", function() {
+//   var timeEl = document.getElementById('time-el');
+//   var secondsLeft = localStorage.getItem('secondsLeft') || 75;
+  
+//   function updateTimer() {
+//       timeEl.textContent = secondsLeft;
+//       if (secondsLeft <= 0) {
+//           clearInterval(timerInterval);
+//           // Call a function or perform an action when the timer reaches 0
+//           window.location.href = 'leaderboard.html';
+//       } else {
+//           secondsLeft--;
+//           // Save the updated secondsLeft value in Local Storage
+//           localStorage.setItem('secondsLeft', secondsLeft);
+//       }
+//   }
+  
+//   function subtractTime(seconds) {
+//       secondsLeft -= seconds;
+//       localStorage.setItem('secondsLeft', secondsLeft);
+//       updateTimer();
+//   }
+  
+//   var timerInterval;
+  
+//   // Start the timer when the page loads
+//   startTimer();
+  
+//   function startTimer() {
+//       updateTimer();
+//       timerInterval = setInterval(updateTimer, 1000);
+//   }
+  
+//   const wrongAnswerButtons = document.querySelectorAll('#wrong-button');
+  
+//   function handleWrongButtonClick() {
+//       subtractTime(15); // Subtract 15 seconds
+//       displayContent();
+//   }
+  
+//   function displayContent() {
+    
+//   }
+  
+//   wrongAnswerButtons.forEach(button => {
+//       button.addEventListener('click', handleWrongButtonClick);
+//   });
+  
+//   const correctBtn = document.getElementById('correct-button');
+//   let score = 0;
+  
+//   if (localStorage.getItem('score')) {
+//       score = parseInt(localStorage.getItem('score'));
+//   }
+  
+//   function updateScoreDisplay() {
+//       // Update the UI element that displays the score
+//       // For example: document.getElementById('score-display').textContent = score;
+//   }
+  
+//   function increaseScore() {
+//       score += 10;
+//       localStorage.setItem('score', score.toString());
+//       updateScoreDisplay();
+//   }
+  
+//   const wrongMessage = document.querySelector('.time');
+//   wrongAnswerButtons.forEach(button => {
+//       button.addEventListener('click', function() {
+//           wrongMessage.style.display = 'block';
+//           setTimeout(function() {
+//               wrongMessage.style.display = 'none';
+//           }, 2000);
+//       });
+//   });
+  
+//   correctBtn.addEventListener('click', increaseScore);
+//   updateScoreDisplay();
+  
+//   const finScoreElement = document.querySelector('.fin-score');
+//   finScoreElement.textContent = `Final score: ${secondsLeft + score}`;
+  
+//   // Your form handling code here
+//   const form = document.querySelector('form');
+//   form.addEventListener('submit', function(event) {
+//       event.preventDefault(); // Prevent the default form submission
+//       addToOL();
+//   });
+  
+//   function addToOL() {
+//       var firstName = document.getElementById("fname").value;
+//       var ol = document.getElementById("nameList");
+//       var li = document.createElement("li");
+//       li.appendChild(document.createTextNode(firstName));
+//       ol.appendChild(li);
+//       document.getElementById("fname").value = ""; // Clear the input field
+//   }
+// });
+
+
 document.addEventListener("DOMContentLoaded", function() {
-  var timeEl = document.getElementById('time-el');
-  var secondsLeft = localStorage.getItem('secondsLeft') || 75;
-  
-  function updateTimer() {
-      timeEl.textContent = secondsLeft;
-      if (secondsLeft <= 0) {
-          clearInterval(timerInterval);
-          // Call a function or perform an action when the timer reaches 0
-          window.location.href = 'leaderboard.html';
-      } else {
-          secondsLeft--;
-          // Save the updated secondsLeft value in Local Storage
-          localStorage.setItem('secondsLeft', secondsLeft);
-      }
-  }
-  
-  function subtractTime(seconds) {
-      secondsLeft -= seconds;
-      localStorage.setItem('secondsLeft', secondsLeft);
-      updateTimer();
-  }
-  
-  var timerInterval;
-  
-  // Start the timer when the page loads
-  startTimer();
-  
-  function startTimer() {
-      updateTimer();
-      timerInterval = setInterval(updateTimer, 1000);
-  }
-  
-  const wrongAnswerButtons = document.querySelectorAll('#wrong-button');
-  
-  function handleWrongButtonClick() {
-      subtractTime(15); // Subtract 15 seconds
-      displayContent();
-  }
-  
-  function displayContent() {
-      // Code for displaying content, not provided in your message
-  }
-  
-  wrongAnswerButtons.forEach(button => {
-      button.addEventListener('click', handleWrongButtonClick);
-  });
-  
-  const correctBtn = document.getElementById('correct-button');
-  let score = 0;
-  
-  if (localStorage.getItem('score')) {
-      score = parseInt(localStorage.getItem('score'));
-  }
-  
-  function updateScoreDisplay() {
-      // Update the UI element that displays the score
-      // For example: document.getElementById('score-display').textContent = score;
-  }
-  
-  function increaseScore() {
-      score += 10;
-      localStorage.setItem('score', score.toString());
-      updateScoreDisplay();
-  }
-  
-  const wrongMessage = document.querySelector('.time');
-  wrongAnswerButtons.forEach(button => {
-      button.addEventListener('click', function() {
-          wrongMessage.style.display = 'block';
-          setTimeout(function() {
-              wrongMessage.style.display = 'none';
-          }, 2000);
-      });
-  });
-  
-  correctBtn.addEventListener('click', increaseScore);
-  updateScoreDisplay();
-  
-  const finScoreElement = document.querySelector('.fin-score');
-  finScoreElement.textContent = `Final score: ${secondsLeft + score}`;
-  
-  // Your form handling code here
-  const form = document.querySelector('form');
-  form.addEventListener('submit', function(event) {
-      event.preventDefault(); // Prevent the default form submission
-      addToOL();
-  });
-  
-  function addToOL() {
-      var firstName = document.getElementById("fname").value;
-      var ol = document.getElementById("nameList");
-      var li = document.createElement("li");
-      li.appendChild(document.createTextNode(firstName));
-      ol.appendChild(li);
-      document.getElementById("fname").value = ""; // Clear the input field
-  }
+    var timeEl = document.getElementById('time-el');
+    var secondsLeft = localStorage.getItem('time-left') || 75;
+    
+    function updateTimer() {
+        timeEl.textContent = secondsLeft;
+        if (secondsLeft <= 0) {
+            clearInterval(timerInterval);
+            // Call a function or perform an action when the timer reaches 0
+            window.location.href = 'C:\Users\Rst14\All-school-projects\What-is-code-quiz\leaderbored.html';
+        } else {
+            secondsLeft--;
+            // Save the updated secondsLeft value in Local Storage
+            localStorage.setItem('time-left', secondsLeft);
+        }
+    }
+    
+    function subtractTime(seconds) {
+        secondsLeft -= seconds;
+        localStorage.setItem('time-left', secondsLeft);
+        updateTimer();
+    }
+    
+    var timerInterval;
+    
+    // Start the timer when the page loads
+    startTimer();
+    
+    function startTimer() {
+        updateTimer();
+        timerInterval = setInterval(updateTimer, 1000);
+    }
+    
+    const wrongAnswerButtons = document.querySelectorAll('#wrong-button');
+    
+    function handleWrongButtonClick() {
+        subtractTime(15); // Subtract 15 seconds
+        displayContent();
+    }
+    
+    
+    wrongAnswerButtons.forEach(button => {
+        button.addEventListener('click', handleWrongButtonClick);
+    });
+    
+    const correctBtn = document.getElementById('correct-button');
+    let score = 0;
+    
+    if (localStorage.getItem('score')) {
+        score = parseInt(localStorage.getItem('score'));
+    }
+    
+    function updateScoreDisplay() {
+        // Update the UI element that displays the score
+        // For example: document.getElementById('score-display').textContent = score;
+    }
+    
+    function increaseScore() {
+        score += 10;
+        localStorage.setItem('score', score.toString());
+        updateScoreDisplay();
+    }
+    
+    const wrongMessages = document.querySelectorAll('.time');
+        wrongAnswerButtons.forEach(button => {
+        button.addEventListener('click', function() {
+        wrongMessages.forEach(wrongMessage => {
+            wrongMessage.style.display = 'block';
+            setTimeout(function() {
+                wrongMessage.style.display = 'none';
+            }, 1000);
+        });
+    });
 });
+    
+    correctBtn.addEventListener('click', increaseScore);
+    updateScoreDisplay();
+    
+    const finScoreElement = document.querySelector('.fin-score');
+    finScoreElement.textContent = `Final score: ${parseInt(secondsLeft) + score}`;
+    
+    
+    const form = document.querySelector('form');
+    form.addEventListener('submit', function(event) {
+        event.preventDefault(); 
+        addToOL();
+    });
+    
+    function addToOL() {
+        var firstName = document.getElementById("fname").value;
+        var ol = document.getElementById("nameList");
+        var li = document.createElement("li");
+        li.appendChild(document.createTextNode(firstName));
+        ol.appendChild(li);
+        document.getElementById("fname").value = ""; // Clear the input field
+    }
+  });
