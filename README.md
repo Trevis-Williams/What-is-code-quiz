696413284b03f4388590551fa8952ed8b8275c70
Trevis Williams <rst1426@gmail.com>
2023-08-25 14:31:48 -0700

    test to see if it will depoly next page

--- README.md
+++ README.md
@@ -1,12 +1,20 @@
 # What-is-code-quiz
 
 ## description 
-This is a simple quiz! There is a leaderbored and a points system. The faster you comple the quiz and the more acrutaly the better you score will be!!!
+This is a simple quiz about coding! There is a sccorebored and a points system. The faster you complete the quiz and the more acrutaly the better you score will be!!!
 
 ## screenshots
-
+![Screenshot 2023-08-25 115112](https://github.com/Trevis-Williams/What-is-code-quiz/assets/135195221/a9c7f752-7619-4f89-955e-04f0fa3eb0ea)
+![Screenshot 2023-08-25 115127](https://github.com/Trevis-Williams/What-is-code-quiz/assets/135195221/3d9bb93c-335c-4805-a704-0f6ff21567de)
+![Screenshot 2023-08-25 115135](https://github.com/Trevis-Williams/What-is-code-quiz/assets/135195221/5a71750b-0cab-4088-888d-c726934e64c1)
+![Screenshot 2023-08-25 115104](https://github.com/Trevis-Williams/What-is-code-quiz/assets/135195221/59f6bd66-eccb-4a21-bab5-03c67051b4c4)
+![Screenshot 2023-08-25 115119](https://github.com/Trevis-Williams/What-is-code-quiz/assets/135195221/d7b7645d-83d7-40b3-9bbd-6c9324a46887)
+![Screenshot 2023-08-25 115151](https://github.com/Trevis-Williams/What-is-code-quiz/assets/135195221/227ef81a-921e-498a-b1a2-da5f884c9c81)
+![Screenshot 2023-08-25 115053](https://github.com/Trevis-Williams/What-is-code-quiz/assets/135195221/06863477-11d1-47b5-b197-0b5ef15e6ae1)
 
 ## Deployed page
 
 
-  
+  ## Help
+  recived help from 
+
--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,105 @@
 
 
+// document.addEventListener("DOMContentLoaded", function() {
+//     var timeEl = document.getElementById('time-el');
+//     var secondsLeft = localStorage.getItem('time-left') || 75;
+    
+//     function updateTimer() {
+//         timeEl.textContent = secondsLeft;
+//         if (secondsLeft <= 0) {
+//             clearInterval(timerInterval);
+//             // Call a function or perform an action when the timer reaches 0
+//             window.location.href = 'C:\\Users\\Rst14\\All-school-projects\\What-is-code-quiz\\leaderbored.html';
+
+//         } else {
+//             secondsLeft--;
+//             // Save the updated secondsLeft value in Local Storage
+//             localStorage.setItem('time-left', secondsLeft);
+//         }
+//     }
+    
+//     function subtractTime(seconds) {
+//         secondsLeft -= seconds;
+//         localStorage.setItem('time-left', secondsLeft);
+//         updateTimer();
+//     }
+    
+//     var timerInterval;
+    
+//     // Start the timer when the page loads
+//     startTimer();
+    
+//     function startTimer() {
+//         updateTimer();
+//         timerInterval = setInterval(updateTimer, 1000);
+//     }
+    
+//     const wrongAnswerButtons = document.querySelectorAll('#wrong-button');
+    
+//     function handleWrongButtonClick() {
+//         subtractTime(15); // Subtract 15 seconds
+//         displayContent();
+//     }
+    
+    
+//     wrongAnswerButtons.forEach(button => {
+//         button.addEventListener('click', handleWrongButtonClick);
+//     });
+    
+//     const correctBtn = document.getElementById('correct-button');
+//     let score = 0;
+    
+//     if (localStorage.getItem('score')) {
+//         score = parseInt(localStorage.getItem('score'));
+//     }
+    
+//     function updateScoreDisplay() {
+//         // Update the UI element that displays the score
+//         // For example: document.getElementById('score-display').textContent = score;
+//     }
+    
+//     function increaseScore() {
+//         score += 10;
+//         localStorage.setItem('score', score.toString());
+//         updateScoreDisplay();
+//     }
+    
+//     const wrongMessages = document.querySelectorAll('.time');
+//         wrongAnswerButtons.forEach(button => {
+//         button.addEventListener('click', function() {
+//         wrongMessages.forEach(wrongMessage => {
+//             wrongMessage.style.display = 'block';
+//             setTimeout(function() {
+//                 wrongMessage.style.display = 'none';
+//             }, 1000);
+//         });
+//     });
+// });
+    
+//     correctBtn.addEventListener('click', increaseScore);
+//     updateScoreDisplay();
+    
+//     const finScoreElement = document.querySelector('.fin-score');
+//     finScoreElement.textContent = `Final score: ${parseInt(secondsLeft) + score}`;
+    
+    
+//     const form = document.querySelector('form');
+//     form.addEventListener('submit', function(event) {
+//         event.preventDefault(); 
+//         addToOL();
+//     });
+    
+//     function addToOL() {
+//         var firstName = document.getElementById("fname").value;
+//         var ol = document.getElementById("nameList");
+//         var li = document.createElement("li");
+//         li.appendChild(document.createTextNode(firstName));
+//         ol.appendChild(li);
+//         document.getElementById("fname").value = ""; // Clear the input field
+//     }
+//   });
+
+
 document.addEventListener("DOMContentLoaded", function() {
     var timeEl = document.getElementById('time-el');
     var secondsLeft = localStorage.getItem('time-left') || 75;
@@ -65,23 +165,22 @@ document.addEventListener("DOMContentLoaded", function() {
     }
     
     const wrongMessages = document.querySelectorAll('.time');
-        wrongAnswerButtons.forEach(button => {
+    wrongAnswerButtons.forEach(button => {
         button.addEventListener('click', function() {
-        wrongMessages.forEach(wrongMessage => {
-            wrongMessage.style.display = 'block';
-            setTimeout(function() {
-                wrongMessage.style.display = 'none';
-            }, 1000);
+            wrongMessages.forEach(wrongMessage => {
+                wrongMessage.style.display = 'block';
+                setTimeout(function() {
+                    wrongMessage.style.display = 'none';
+                }, 1000);
+            });
         });
     });
-});
     
     correctBtn.addEventListener('click', increaseScore);
     updateScoreDisplay();
     
     const finScoreElement = document.querySelector('.fin-score');
     finScoreElement.textContent = `Final score: ${parseInt(secondsLeft) + score}`;
-    
     
     const form = document.querySelector('form');
     form.addEventListener('submit', function(event) {
@@ -97,4 +196,4 @@ document.addEventListener("DOMContentLoaded", function() {
         ol.appendChild(li);
         document.getElementById("fname").value = ""; // Clear the input field
     }
-  });
+});
